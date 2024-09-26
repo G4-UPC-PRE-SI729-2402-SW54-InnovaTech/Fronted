36f11192f7a2dba03263dd8e474859e129152f46
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-09-26 16:03:53 -0500

    feat(sidenav):add sidenav v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {ToolbarComponent} from "./toolbar/toolbar.component";
 import {NgIf} from "@angular/common";
+import {SidebarComponent} from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent, NgIf],
+  imports: [RouterOutlet, ToolbarComponent, NgIf, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
