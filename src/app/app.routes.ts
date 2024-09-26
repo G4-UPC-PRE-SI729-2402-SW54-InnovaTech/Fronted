cd3cf9d3a208e8b5682b8bc8b131f1559bb63553
sebasepe <u202125968@upc.edu.pe>
2024-09-25 21:35:15 -0500

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import {ForgotPasswordCodeComponent} from "./forgot-password-code/forgot-password-code.component";
-import {ForgotPasswordNewComponent} from "./forgot-password-new/forgot-password-new.component";
+//import {ForgotPasswordCodeComponent} from "./forgot-password-code/forgot-password-code.component";
+//import {ForgotPasswordNewComponent} from "./forgot-password-new/forgot-password-new.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'forgot-password-code', component: ForgotPasswordCodeComponent },
-  { path: 'forgot-password-new', component: ForgotPasswordNewComponent },
+  //{ path: 'forgot-password-code', component: ForgotPasswordCodeComponent },
+  //{ path: 'forgot-password-new', component: ForgotPasswordNewComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
