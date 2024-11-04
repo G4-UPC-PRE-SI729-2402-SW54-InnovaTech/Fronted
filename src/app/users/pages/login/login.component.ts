cbcdae14a5d59a5bbf7b98446785aca08a41b823
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-11-03 20:06:00 -0500

    feat: user validation and dashboard v2

--- src/app/users/pages/login/login.component.ts
+++ src/app/users/pages/login/login.component.ts
@@ -1,24 +1,39 @@
 import { Component } from '@angular/core';
-import {MatCardImage} from "@angular/material/card";
-import {Router} from "@angular/router";
+import { MatCardImage } from "@angular/material/card";
+import { Router } from "@angular/router";
+import {ConnexApiService} from "../../../shared/services/connex-api.service";
+import {FormsModule} from "@angular/forms";
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-    imports: [
-        MatCardImage
-    ],
+  imports: [
+    MatCardImage,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private apiService: ConnexApiService) {}
 
   onSendCode(): void {
-    this.router.navigate(['/user-interface-main']);
+    this.apiService.getUserByEmail(this.email).subscribe((user: any) => {
+      if (user && user.passwordHash === this.password) {
+        this.router.navigate(['/user-interface-main']);
+      } else {
+        alert('Invalid email or password');
+      }
+    }, error => {
+      alert('Error fetching user data');
+    });
   }
+
   toRegister(): void {
     this.router.navigate(['/register']);
   }
-
 }
